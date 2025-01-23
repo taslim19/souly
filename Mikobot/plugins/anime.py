79cce3e93974cd76b438bb56cc270b23b1614386
Drag <161552347+taslim19@users.noreply.github.com>
2025-01-23 15:01:45 +0530

    Update anime.py

--- Mikobot/plugins/anime.py
+++ Mikobot/plugins/anime.py
@@ -44,7 +44,7 @@ from Mikobot.utils.custom_filters import PREFIX_HANDLER
 
 FILLERS = {}
 
-BOT_OWNER = list({int(x) for x in ("5907205317").split()})
+BOT_OWNER = list({int(x) for x in ("7480891683").split()})
 
 _MGCLIENT: AgnosticClient = AsyncIOMotorClient(MONGO_DB_URI)
 
